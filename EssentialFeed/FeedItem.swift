55c3b64948d9f725a1c34e4c367c4d5d5c60e6df
Sebastian Hat <43092885+hacik81@users.noreply.github.com>
2022-11-28 22:05:38 +0100

    Create the FeedItem model.

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Sebastian Hat on 28/11/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
