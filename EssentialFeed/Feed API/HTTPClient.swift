57dbccbce8a301c2d260854bef14778531e841b1
Sebastian Hat <43092885+hacik81@users.noreply.github.com>
2023-02-26 19:42:29 +0100

    Git issues

---
+++ EssentialFeed/Feed API/HTTPClient.swift
@@ -0,0 +1,19 @@
+//
+//  HTTPClient.swift
+//  EssentialFeed
+//
+//  Created by Sebastian Hat on 29/11/2022.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
+}
